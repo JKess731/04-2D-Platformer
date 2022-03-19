a9e00e8d51ce2b145d2e076bb58b787c620b5696
JKess731 <62125526+JKess731@users.noreply.github.com>
2022-03-19 17:27:54 -0400

    Initial commit

---
+++ Global.gd
@@ -0,0 +1,88 @@
+extends Node
+
+const SAVE_PATH = "user://savegame.sav"
+const SECRET = "C220 Is the Best!"
+var save_file = ConfigFile.new()
+
+onready var HUD = get_node_or_null("/root/Game/UI/HUD")
+onready var Coins = get_node_or_null("/root/Game/Coins")
+onready var Mines = get_node_or_null("/root/Game/Mines")
+onready var Game = load("res://Game.tscn")
+onready var Coin = load("res://Coin/Coin.tscn")
+onready var Mine = load("res://Mine/Mine.tscn")
+
+var save_data = {
+	"general": {
+		"score":0
+		,"health":100
+		,"coins":[]
+		,"mines":[]	
+	}
+}
+
+
+var fade = null
+var fade_speed = 0.015
+
+var fade_in = false
+var fade_out = ""
+
+var death_zone = 1000
+
+func _physics_process(_delta):
+	if fade == null:
+		fade = get_node_or_null("/root/Game/Camera/Fade")
+	if fade_out != "":
+		execute_fade_out(fade_out)
+	if fade_in:
+		execute_fade_in()
+		
+
+func start_fade_in():
+	if fade != null:
+		fade.visible = true
+		fade.color.a = 1
+		fade_in = true
+
+func start_fade_out(target):
+	if fade != null:
+		fade.color.a = 0
+		fade.visible = true
+		fade_out = target
+
+func execute_fade_in():
+	if fade != null:
+		fade.color.a -= fade_speed
+		if fade.color.a <= 0:
+			fade_in = false
+
+func execute_fade_out(target):
+	if fade != null:
+		fade.color.a += fade_speed
+		if fade.color.a >= 1:
+			fade_out = ""
+
+func save_game():
+	save_data["general"]["coins"] = []					# creating a list of all the coins and mines that appear in the scene
+	save_data["general"]["mines"] = []
+
+	var save_game = File.new()						# create a new file object
+	save_game.open_encrypted_with_pass(SAVE_PATH, File.WRITE, SECRET)	# prep it for writing to, make sure the contents are encrypted
+	save_game.store_string(to_json(save_data))				# convert the data to a json representation and write it to the file
+	save_game.close()							# close the file so other processes can read from or write to it
+	
+func load_game():
+	var save_game = File.new()						# Create a new file object
+	if not save_game.file_exists(SAVE_PATH):				# If it doesn't exist, skip the rest of the function
+		return
+	save_game.open_encrypted_with_pass(SAVE_PATH, File.READ, SECRET)	# The file should be encrypted
+	var contents = save_game.get_as_text()					# Get the contents of the file
+	var result_json = JSON.parse(contents)					# And parse the JSON
+	if result_json.error == OK:						# Check to make sure the JSON got successfully parsed
+		save_data = result_json.result_json				# If so, load the data from the file into the save_data lists
+	else:
+		print("Error: ", result_json.error)
+	save_game.close()							# Close the file so other processes can read from or write to it
+	
+	var _scene = get_tree().change_scene_to(Game)				# Load the scene
+	call_deferred("restart_level")						# When it's done being loaded, call the restart_level method
